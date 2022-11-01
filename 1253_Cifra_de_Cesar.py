502aec74ef87ee9367bfe915d9fe82cf4d3d564f
MatheusMnz <77499300+MatheusMnz@users.noreply.github.com>
2022-11-01 15:37:45 -0300

    Update 1253_Cifra_de_Cesar.py

--- 1253_Cifra_de_Cesar.py
+++ 1253_Cifra_de_Cesar.py
@@ -2,15 +2,15 @@
 
 
 #Casos de teste
-N = int(input());
+N = int(input())
 
 for i in range(0,N):
-    palavra = input();
+    palavra = input()
     quantity = int(input())
     new_word = ''
     for j in palavra:
         posicao = ord(j) - quantity
-        if(posicao < 65):
+        if posicao < 65:
             new_word += chr(91-(65-posicao))
         else:
             new_word += chr(ord(j)-quantity)
